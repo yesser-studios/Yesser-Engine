c20572f5134b0a16613e6f6e6ddfdce9d0d6d17d
yesseruser <yesseruseryt@gmail.com>
2023-12-29 00:09:36 +0100

    Added a UWP project template.

--- YesserEngine.Templates.UWP/Properties/AssemblyInfo.cs
+++ YesserEngine.Templates.UWP/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YesserEngine.Templates.UWP")]
+[assembly: AssemblyTitle("test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Yesseruser")]
-[assembly: AssemblyProduct("YesserEngine.Templates.UWP")]
-[assembly: AssemblyCopyright("Copyright © Yesseruser 2023")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d6ffa94a-a386-407f-a016-cd71f1dfdb0b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
