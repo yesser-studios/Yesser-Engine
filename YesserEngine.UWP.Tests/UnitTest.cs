8a34382b38ebaa22200cce12398bb5122a479fa9
yesseruser <yesseruseryt@gmail.com>
2023-12-29 01:07:43 +0100

    Added an UWP test project.

---
+++ YesserEngine.UWP.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YesserEngine.UWP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ()
+        {
+        }
+    }
+}
