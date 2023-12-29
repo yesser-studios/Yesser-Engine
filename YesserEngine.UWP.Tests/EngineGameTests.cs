59a2b442bd6e9bd221a344b49d476bff978b880f
yesseruser <yesseruseryt@gmail.com>
2023-12-29 01:10:30 +0100

    Added a engine game constructor test.

---
+++ YesserEngine.UWP.Tests/EngineGameTests.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YesserStudios.Engine;
+
+namespace YesserEngine.UWP.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EngineGameConstructorWorks()
+        {
+            var game = new EngineGame();
+
+            Assert.IsNotNull(game);
+            Assert.IsInstanceOfType(game, typeof(EngineGame));
+        }
+    }
+}
